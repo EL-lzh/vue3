0d94f1ac020134e4cf5081c4cf6be93d8ac71500
刘泽洪 <liuzehong1@szkingdom.com>
2023-10-24 00:16:48 +0800

    feat: 初始化项目

---
+++ scripts/build.js
@@ -0,0 +1,9 @@
+// import path from 'node:path'
+
+const fs = require('fs')
+const dirs = fs.readdirSync('packages')
+// const pkgDir = path.resolve(`packages`)
+console.log(dirs)
+console.log('---------')
+// console.log(pkgDir)
+console.log('---------')
