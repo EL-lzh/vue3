b4f1ad633b4d6f3fffa1722c0e1f7c34b7ec2e88
刘泽洪 <liuzehong1@szkingdom.com>
2023-10-24 23:33:21 +0800

    feat: 并行打包基本代码

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,40 @@
 // import path from 'node:path'
-
 const fs = require('fs')
-const dirs = fs.readdirSync('packages')
+const execa = require('execa')
+
+const dirs = fs.readdirSync('packages').filter(p=>{
+  if(!fs.statSync(`packages/${p}`).isDirectory()) {
+    return false
+  }
+  return true
+})
+async function build(target) {
+  console.log('--' + target)
+  // execa包--开启子进程
+  // -c 表示执行rollup配置(自定义的)，
+  // 环境变量
+  await execa('rollup',['-c', '--environment', `TARGET:${target}`])
+}
+/**
+ * 打包  并行打包
+ * @param {*} source packages目录子模块
+ * @param {*} itemfn 打包的方法
+ * @returns 
+ */
+async function runParallel(source, itemfn) {
+
+  const result = []
+  for (const item of source) {
+    result.push(itemfn(item))
+  }
+
+  return Promise.all(result)
+
+}
+
+runParallel(dirs, build).then(()=>{
+  console.log('成功')
+})
 // const pkgDir = path.resolve(`packages`)
 console.log(dirs)
 console.log('---------')
